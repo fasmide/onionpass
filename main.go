e6bf1671af312a0ad8c58c5660c4cca810a4f672
Kristian Mide <fas@mide.dk>
2019-09-04 20:43:50 +0200

    Added simple http redirect

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/fasmide/onionpass/ssh"
 	"golang.org/x/net/proxy"
 )
 
 var sshPort = flag.Int("sshport", 0, "ssh listen port")
+var httpPort = flag.Int("httpport", 0, "http listen port")
 
 func main() {
 	flag.Parse()
+
+	// lets do a webserver which redirects anyone to the project
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "https://github.com/fasmide/onionpass", http.StatusFound)
+	})
+
+	go func() {
+		l := fmt.Sprintf(":%d", *httpPort)
+		log.Print("http listening on ", l)
+		log.Printf("http server failed: %s", http.ListenAndServe(l, nil))
+		// we dont care if this http server fails
+	}()
 
 	sshConfig, err := ssh.DefaultConfig()
 	if err != nil {
